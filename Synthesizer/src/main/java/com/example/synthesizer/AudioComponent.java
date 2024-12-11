a3cba1fa75017a1ec9da2cda8d42c7643f3005cf
Yutian Qin <yutianqin2000@gmail.com>
2024-12-11 16:15:17 -0700

    Delete Synthesizer directory

--- Synthesizer/src/main/java/com/example/synthesizer/AudioComponent.java
+++
@@ -1,9 +0,0 @@
-package com.example.synthesizer;
-
-
-public interface AudioComponent {
-    AudioClip getClip();
-    default boolean hasInput() { return false; }
-    default void connectInput(AudioComponent input) { assert( false ); }
-    default void removeInput(AudioComponent input) { assert( false ); };
-}
